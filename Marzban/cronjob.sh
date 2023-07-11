167258f6e6b04a7f985bbfc0d69a3e5af1f4546a
Houshmand <houshmand2005@gmail.com>
2023-07-11 11:04:18 +0330

    update cronjob

--- Marzban/cronjob.sh
+++ Marzban/cronjob.sh
@@ -1,12 +1,22 @@
 #!/bin/bash
 
-echo "Script executed at $(date)" >> /root/V2IpLimit/Marzban/cron_start.log
+echo "Script executed at $(date)" >> /root/V2IpLimit/Marzban/cronjob_log.log
 cd /root/V2IpLimit/Marzban/
 
-if pgrep -f /root/V2IpLimit/Marzban/v2_ip_limit.py > /dev/null; then
-    pkill -f /root/V2IpLimit/Marzban/v2_ip_limit.py
-    sleep 3
-    python3 /root/V2IpLimit/Marzban/v2_ip_limit.py
-else
-    python3 /root/V2IpLimit/Marzban/v2_ip_limit.py
+# Terminate existing instances of v2_ip_limit.py
+echo "Terminating existing instances of v2_ip_limit.py" >> cronjob_log.log
+pids=$(pgrep -f v2_ip_limit.py)
+if [ -n "$pids" ]; then
+    echo "Existing process IDs: $pids" >> cronjob_log.log
+    for pid in $pids; do
+        echo "Killing process $pid" >> cronjob_log.log
+        kill -9 "$pid"
+    done
+    sleep 5
+    pids=$(pgrep -f v2_ip_limit.py)
+    echo "Remaining process IDs: $pids" >> cronjob_log.log
 fi
+
+# Start v2_ip_limit.py
+echo "Starting v2_ip_limit.py" >> cronjob_log.log
+python3 v2_ip_limit.py
