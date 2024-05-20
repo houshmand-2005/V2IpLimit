2d16db7d3f8677415a5e65cef244ff8bfb642f87
houshmand-2005 <houshmand2005@gmail.com>
2024-05-20 18:31:07 +0330

    Update maxBytes in file_handler to 7MB

--- utils/logs.py
+++ utils/logs.py
@@ -9,8 +9,8 @@ from logging.handlers import RotatingFileHandler
 logger = logging.getLogger()
 logger.setLevel(logging.INFO)
 file_handler = RotatingFileHandler(
-    "app.log", maxBytes=5 * 10**6, backupCount=3
-)  # 5MB per file, keep 3 old files
+    "app.log", maxBytes=7 * 10**6, backupCount=3
+)  # 7MB per file, keep 3 old files
 file_handler.setLevel(logging.INFO)
 stream_handler = logging.StreamHandler()
 stream_handler.setLevel(logging.ERROR)
